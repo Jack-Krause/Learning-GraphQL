746810b3539a332a7f5b166ba891c3aa3ee430bd
Jack Krause <jmkrause@iastate.edu>
2025-08-20 13:28:46 -0500

    mutations completed - queries to be added

--- server.js
+++ server.js
@@ -48,7 +48,29 @@ const typeDefs = `#graphql
       starship(id: ID!): Starship
       character(id: ID!): Character
       characters: [Character!]!
+      reviews(episode: Episode!): [Review!]!
+      allReviews: [Review!]!
     }
+
+
+    type Review {
+      episode: Episode!
+      stars: Int!
+      comment: String
+    }
+
+    input ReviewInput {
+      stars: Int!
+      comment: String
+    }
+
+    type Mutation {
+      createReview(episode: Episode!, review: ReviewInput!): Review!
+      updateReviewStars(episode: Episode!, stars: Int!, comment: String): Review!
+      deleteCharacter(id: ID!): ID!
+    }
+
+
 `;
 
 const starships = [
@@ -95,12 +117,27 @@ const droids = [
 ];
 
 
-const allCharacters = [
-    ...humans.map(h => ({ ...h, __type: "Human"})),
-    ...droids.map(d => ({ ...d, __type: "Droid"}))
+const reviews = [
+  { episode: "NEWHOPE", stars: 0, comment: "a new hope" },
+  { episode: "EMPIRE",  stars: 0, comment: "the empire strikes back" },
+  { episode: "JEDI",    stars: 0, comment: "return of the jedi" }
 ];
 
-const byId = new Map(allCharacters.map(c => [c.id, c]));
+
+const getAllCharacters = () => ([
+  ...humans.map(h => ({...h, __type: "Human"})),
+  ...droids.map(d => ({...d, __type: "Droid"})),
+]);
+
+
+const getCharacterById = (id) => {
+  const h = humans.find(x => x.id === id);
+  if (h) return {...h, __type: "Human"};
+  const d = droids.find(x => x.id === id);
+  if (d) return {...d, __type: "Droid"};
+  return null;
+}
+
 const shipById = new Map(starships.map(s => [s.id, s]));
 
 // const allCharacters = [...humans.map(h => ({ ...h, __type: "Human" })), ...droids.map(d => ({ ...d, __type: "Droid" }))];
@@ -113,8 +150,70 @@ const resolvers = {
   Query: {
     // Called for: query { starship(id: "...") { ... } }
     starship: (_, { id }) => starships.find(s => s.id === id) || null,
-    character: (_, { id }) => byId.get(id) || null,
-    characters: () => allCharacters
+    character: (_, { id }) => getCharacterById(id),
+    characters: () => getAllCharacters(),
+    reviews: (_, { episode }) => reviews.filter(review => review.episode === episode),
+    allReviews: () => reviews,
+  },
+
+  Mutation: { 
+    createReview: (_, { episode, review }) => {
+      if (review.stars < 0 || review.stars > 5) {
+        throw new Error("invalid number of stars [0, 5]");
+      }
+
+      const r = {
+        episode,
+        stars: review.stars,
+        comment: review.comment ?? null
+      }
+
+      reviews.push(r);
+      console.info(`Review updated for ${episode} to ${r.stars} stars.`)
+      return r;
+    },
+
+    updateReviewStars: (_, { episode, stars, comment }) => {
+      if (stars < 0 || stars > 5) {
+        throw new Error("invalid number of stars [0, 5]");
+      }
+
+      const r = reviews.find((rev) => rev.episode === episode);
+      if (! r) {
+        throw new Error(`no review found for episode: ${episode}`);
+      }
+
+      r.stars = stars;
+      if (typeof comment !== "undefined") {
+        r.comment = comment;
+      }
+
+      return r;
+    },
+
+    deleteCharacter: (_, {id}) => {
+      const hIdx = humans.findIndex(h => h.id === id);
+      const dIdx = droids.findIndex(d => d.id === id);
+
+      if (hIdx === -1 && dIdx === -1) {
+        throw new Error(`no character found with id: ${id}`);
+      }
+
+      if (hIdx !== -1) humans.splice(hIdx, 1);
+      if (dIdx !== -1) droids.splice(dIdx, 1);
+
+      for (const h of humans) {
+        h.friendIds = (h.friendIds || []).filter(fid => fid !== id);
+      }
+
+      for (const d of droids) {
+        d.friendIds = (d.friendIds || []).filter(fid => fid !== id);
+      }
+      
+      return id;
+    },
+    
+
   },
 
   Starship: {
@@ -135,12 +234,12 @@ const resolvers = {
   },
 
   Human: {
-    friends: (human) => human.friendIds?.map(id => byId.get(id)) || [],
+    friends: (human) => (human.friendIds || []).map(getCharacterById),
     starships: (human) => human.starshipIds?.map(id => shipById.get(id)) || []
   },
 
   Droid: {
-    friends: (droid) => droid.friendIds?.map(id => byId.get(id)) || [],
+    friends: (droid) => (droid.friendIds || []).map(getCharacterById),
   }
 
 };
